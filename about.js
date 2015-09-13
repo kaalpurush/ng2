99df15adfbf9f728fcebd87ca71c02f9a388c8ef
kaalpurush <kaalbd@gmail.com>
2015-09-14 02:38:05 +0600

    form validation

--- about.js
+++ about.js
@@ -11,18 +11,36 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 };
 var angular2_1 = require('angular2/angular2');
 var router_1 = require('angular2/router');
+var angular2_2 = require("angular2/angular2");
 var About = (function () {
-    function About(params) {
+    function About(params, fb) {
         this.id = params.get('id');
+        this.loginForm = fb.group({
+            email: ['', angular2_2.Validators.compose([angular2_2.Validators.required, this.invalidEmail])],
+            password: ['', angular2_2.Validators.required]
+        });
     }
+    About.prototype.invalidEmail = function (control) {
+        if (!control.value.match(/^[_a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/)) {
+            return { invalidEmail: true };
+        }
+    };
+    About.prototype.doLogin = function (event) {
+        console.log(this.loginForm.value);
+        event.preventDefault();
+    };
     About = __decorate([
         angular2_1.Component({
-            selector: 'about'
+            selector: 'about',
+            viewBindings: [angular2_2.FormBuilder]
         }),
         angular2_1.View({
-            templateUrl: 'about.html'
+            templateUrl: 'about.html',
+            directives: [
+                angular2_2.FORM_DIRECTIVES,
+            ]
         }), 
-        __metadata('design:paramtypes', [router_1.RouteParams])
+        __metadata('design:paramtypes', [router_1.RouteParams, angular2_2.FormBuilder])
     ], About);
     return About;
 })();
--- about.ts
+++ about.ts
@@ -1,18 +1,38 @@
 import {Component, View} from 'angular2/angular2';
-
 import {RouteParams} from 'angular2/router';
+import {FormBuilder, Validators, ControlGroup, FORM_DIRECTIVES} from "angular2/angular2";
 
 @Component({
-    selector: 'about'
+    selector: 'about',
+    viewBindings: [FormBuilder]
 })
 
 @View({
-    templateUrl: 'about.html'
+    templateUrl: 'about.html',
+    directives: [
+        FORM_DIRECTIVES, 
+    ]
 })
 
 export class About {
     id: string;
-    constructor(params: RouteParams){
+    loginForm: ControlGroup;
+    constructor(params: RouteParams, fb: FormBuilder){
         this.id = params.get('id');
+        this.loginForm = fb.group({
+            email: ['', Validators.compose([Validators.required, this.invalidEmail ])],
+            password: ['', Validators.required]
+        });
+    }
+    
+    invalidEmail(control) {  
+        if (!control.value.match(/^[_a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/)){  
+            return {invalidEmail: true};  
+        }
+    }
+    
+    doLogin(event) {
+        console.log(this.loginForm.value);    
+        event.preventDefault();
     }
 }
